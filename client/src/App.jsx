334f8c102b7776ddb548c7d5e2803cb5d235344e
chsainikhilreddy <bennikhilreddy@gmail.com>
2022-02-16 19:32:08 +0530

    Integrated leaderboard, login/signup page.

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from './components/HomePage';
+import Parent from './components/parent';
+import UpdateProfile from './components/UpdateProfile';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={["/", "/login", "/signup"]} component={HomePage} />
+
+        <Route exact path={["/dashboard"]} component={Parent} />
+
+        <Route exact path={["/update"]} component={UpdateProfile} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
